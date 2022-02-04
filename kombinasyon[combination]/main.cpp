5b78bc4a864f468349b8cdff66b5298d31e2fa57
c-kazanci <coskunkazanci@uludag.edu.tr>
2022-02-05 00:59:04 +0300

    Combination code

---
+++ kombinasyon[combination]/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//decleration function
+int fk(int x);
+
+int main(){
+  int result=0, n=8, r=5;
+
+  result = fk(n) / (fk(r) * fk(n-r));
+  cout << "\nThe result : " << result;
+
+  return 0;
+}
+//body of funtion
+int fk(int x){
+  int result=1;
+  for (int i = 1; i <= x; i++){
+    result *= i;
+  }
+  return result;
+}
